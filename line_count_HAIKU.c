65d69c43cd4893c4eba7c0ca3d981d4b55ee790c
kudo <kudo@techmatrix.co.jp>
2025-06-18 10:23:02 +0900

    kernel

--- line_count_HAIKU.c
+++ line_count_HAIKU.c
@@ -274,6 +274,23 @@ void count_menu(void)
     }
 }
 
+
+void fastcall
+prepare_to_wait(wait_queue_head_t *q, wait_queue_t *wait, int state)
+{
+    unsigned long flags;
+
+    wait->flags &= ~WQ_FLAG_EXCLUSIVE;
+    spin_lock_irqsave(&q->lock, flags);
+    if (list_empty(&wait->task_list))
+        __add_wait_queue(q, wait);
+    /*
+     * don't alter the task state if this is just going to
+     * queue an async wait queue callback
+     */
+   if (is_sync_wait(wait)) ;
+}
+
 /*******************************************************************************
 * MODULE        : count_line
 * ABSTRACT      : ライン数カウント実行関数
