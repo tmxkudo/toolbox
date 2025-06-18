850135220bffb7a4924213a47aa9be01192ea853
kudo <kudo@techmatrix.co.jp>
2025-06-18 10:01:43 +0900

    ssl

--- test-data/linux_snippet.c
+++ test-data/linux_snippet.c
@@ -1,7 +1,22 @@
 void fastcall
-prepare_to_wait(wait_queue_head_t *q, wait_queue_t *wait, int state)
+prepare_to_wait(wait_queue_head_t *q, wait_queue_t *wait, int state, SSL *s)
 {
     unsigned long flags;
+
+     pitem *item;
+     hm_fragment *frag;
+     int al;
+ 
+     *ok = 0;
+     item = pqueue_peek(s->d1->buffered_messages);
+     if (item == NULL)
+         return 0;
+ 
+     frag = (hm_fragment *)item->data;
+ 
+     /* Don't return if reassembly still in progress */
+     if (frag->reassembly != NULL)
+         return 0;
 
     wait->flags &= ~WQ_FLAG_EXCLUSIVE;
     spin_lock_irqsave(&q->lock, flags);
